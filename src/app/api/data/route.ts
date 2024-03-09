e8eb1b05e23fd8082e82a2113db793a93356605b
Calvin Yu <yucalvin5@gmail.com>
2024-03-08 16:35:36 -0800

    data retrieved using access token. Working on displaying the data

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -2,7 +2,19 @@
 import { NextRequest } from "next/server";
 
 export async function POST(request: NextRequest) {
-    const data = await request.json();
-    console.log(data);
+    const token = await request.json();
+    const authorization = 'Bearer ' + token.access_token;
+    try {
+        const response = await fetch('https://api.spotify.com/v1/me/top/tracks', {
+            headers: {
+                Authorization: authorization
+            }
+        });
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+        return new Response('Unauthorized', { status: 401 });
+    }
     return new Response('Done', { status: 200 });
 }
